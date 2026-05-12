0a08a7e638632fb36a619f9641001e98074988e4
Vitória Maria Bezerra Pinheiro <vitoriamariabezerra72@gmail.com>
2026-05-12 17:42:05 +0000

    Arquivo de usuario criado com sucesso

---
+++ CoreStudy/usuarios.py
@@ -0,0 +1,18 @@
+
+def cadastrarUsuario():
+    print('------ CADASTRO ------')
+    nome = input('Digite seu nome completo: ')
+    email = input('Digite seu email: ')
+    senha = input('Digite sua senha: ')
+    senha = input('Digite sua senha novamente: ')
+    print('Usuário cadastrado com sucesso!')
+
+cadastrarUsuario()
+
+def logarUsuario():
+    print('------ LOGIN ------')
+    email = input('Digite seu email: ')
+    senha = input('Digite sua senha: ')
+    print('Login realizado com sucesso com sucesso!')
+
+logarUsuario()
